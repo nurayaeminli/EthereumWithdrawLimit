2c275cdde8fd89ddb717af1a8e266253da3e73b4
nurayaeminli <eminli.nuraye@icloud.com>
2024-10-15 05:43:56 +0400

    .

---
+++ contracts/EthereumWithdrawLimit.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract lockedfund {
+    address public owner;
+    uint public timelimit = 30 days;
+
+    mapping(address => uint) public lockedFunds;
+    mapping(address => uint) public lockedTimeStamps;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function lockFunds() public payable {
+        require(msg.value > 0);
+        require(lockedFunds[msg.sender] == 0);
+        lockedFunds[msg.sender] = msg.value;
+        lockedTimeStamps[msg.sender] = block.timestamp;
+    }
+
+    function releaseFunds() public {
+        require(lockedFunds[msg.sender] > 0, "there is no fund to withdraw");
+        require(block.timestamp >= lockedTimeStamps[msg.sender] + timelimit, "Please wait 1 month to withdraw");
+        uint amount = lockedFunds[msg.sender];
+        lockedFunds[msg.sender] = 0;
+        lockedTimeStamps[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    
+    function withdraw() public {
+        require(msg.sender == owner, "Only owner can withdraw the fund");
+        require(address(this).balance > 0, "No funds available to withdraw");
+
+        payable(owner).transfer(address(this).balance);
+    }
+}
